075e5f7cb6cb6bd675822c8837d81a0e78609f41
tdyomina <108633290+tdyomina@users.noreply.github.com>
2022-08-11 23:18:41 +0300

    "current" button fixed

--- src/script.js
+++ src/script.js
@@ -23,12 +23,33 @@ h4.innerHTML = `${day}, ${hours}:${minutes}`;
 
 // Change City from form input
 
+function replaceCityName(response) {
+  let cityName = response.data.name;
+  console.log(cityName);
+  let header = document.querySelector("#selectedCity");
+  header.innerHTML = cityName;
+  let temperature = Math.round(response.data.main.temp);
+  console.log(temperature);
+  let h1 = document.querySelector("#figure");
+  h1.innerHTML = `${temperature}`;
+  let humidity = document.querySelector("#humidity");
+  let humidityValue = response.data.main.humidity;
+  humidity.innerHTML = humidityValue;
+  let windSpeed = document.querySelector("#wind");
+  let windSpeedValue = response.data.wind.speed;
+  windSpeed.innerHTML = windSpeedValue;
+}
+
+function searchCity(cityInput) {
+  let apiKey = "449b625b92babac43b47ea470588167d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(replaceCityName);
+}
+
 function showCity(event) {
   event.preventDefault();
-  let cityInput = document.getElementById("exampleInputCity");
-  let h2 = document.querySelector("#selectedCity");
-  h2.innerHTML = cityInput.value;
-  return cityInput.value;
+  let cityInput = document.getElementById("exampleInputCity").value; // input value
+  searchCity(cityInput);
 }
 let cityName = document.querySelector("#enterCity");
 cityName.addEventListener("click", showCity);
@@ -50,25 +71,9 @@ function handlePosition(position) {
   let apiKey = "449b625b92babac43b47ea470588167d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   console.log(apiUrl);
-  axios.get(`${apiUrl}`).then(showTemp);
+  axios.get(`${apiUrl}`).then(replaceCityName);
 }
 
-function showTemp(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let cityName = response.data.name;
-  console.log(cityName);
-  let header = document.getElementsByClassName("selectedCity");
-  header.innerHTML = response.data.name;
-  console.log(temperature);
-  let h1 = document.querySelector("#figure");
-  h1.innerHTML = `${temperature}`;
-  let humidity = document.querySelector("#humidity");
-  let humidityValue = response.data.main.humidity;
-  humidity.innerHTML = humidityValue;
-  let windSpeed = document.querySelector("#wind");
-  let windSpeedValue = response.data.wind.speed;
-  windSpeed.innerHTML = windSpeedValue;
-}
 
 // farenheit calculation
 
