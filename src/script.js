5c23a19fcf5cb59ad2881b154032dbd72d3a4a2b
tdyomina <108633290+tdyomina@users.noreply.github.com>
2022-08-27 19:43:08 +0300

    Forecast min/max values was added

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ let minutes = now.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
 }
-h4.innerHTML = `${day}, ${hours}:${minutes}`;
+h4.innerHTML = `Last updated: ${day}, ${hours}:${minutes}`;
 
 // Change City from form input
 
@@ -43,6 +43,80 @@ function replaceCityName(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconBlock.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
+}
+
+// Forecast block
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `
+  <div class="row">
+      <div class="col-2">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2370/2370266.png"
+          alt="weather icon"
+          width="50"
+        />
+        <div>
+          </br>
+        </div>        
+        <div>
+          <p>max</p>
+          <p>eve</p>
+          <p>min</p>
+        </div>
+      </div>
+  `;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt="weather icon"
+          width="50"
+        />
+        <div class="weather-forecast-temperatures">
+          <p class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}°C</p>
+          <p class="weather-forecast-temperature-eve"> ${Math.round(
+            forecastDay.temp.eve
+          )}°C</p>
+          <p class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}°C</p>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "449b625b92babac43b47ea470588167d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showCity(event) {
@@ -50,23 +124,6 @@ function showCity(event) {
   let cityInput = document.querySelector("#exampleInputCity").value;
   searchCity(cityInput);
 }
-
-// farenheit calculation
-
-// document
-//   .getElementById("celsium")
-//   .addEventListener("click", function celsiumValue(event) {
-//     event.preventDefault();
-//     let celsiumTemp = response.data.temp;
-//     document.querySelector("#figure").innerHTML = celsiumTemp.value;
-//   });
-// document
-//   .getElementById("farenheit")
-//   .addEventListener("click", function (event) {
-//     event.preventDefault();
-//     let farenheitTemp = Math.round((response.data.main.temp * 9) / 5 + 32);
-//     document.querySelector("#figure").innerHTML = farenheitTemp;
-//   });
 
 // current button processing
 function yourCity(event) {
